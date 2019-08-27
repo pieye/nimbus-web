c010c4fd1b706780caa7b571d0f8e72fce4e4a3d
Markus Proeller <markus.proeller@pieye.org>
2019-08-27 11:41:23 +0100

    sliders removed

--- app.js
+++ app.js
@@ -9,22 +9,24 @@ var NB3D_docElement = document.getElementById( 'container3D' );
 var n3DRender = new Nimbus3DRender(NB3D_docElement);
 var nimbusRPC = new NimbusRPC(location.host);
 
+/*
 var slider = document.getElementById("expoSlider");
 slider.onchange = function() {
     nimbusRPC.setExposure(this.value);
 }
+*/
 
 var statusbar = document.getElementById("statusbar");
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    getExpoSlider();
+    //getExpoSlider();
 });
 
 let cb = function onNewData(header, ampl_arr, dist_arr, conf, x_arr, y_arr, z_arr)
 {
     var plotMin = 0;
     var plotMax = 0;
-    statusbar.innerHTML = "Temp: " + (header[9]/10).toFixed(1) + "&deg;C | fps: " + header[8].toFixed(1);
+    //statusbar.innerHTML = "Temp: " + (header[9]/10).toFixed(1) + "&deg;C | fps: " + header[8].toFixed(1);
     n3DRender.render3Dscene( x_arr, y_arr, z_arr, dist_arr, conf, plotMin, plotMax );
     render2Dscene( ampl_arr, conf, plotMin, plotMax);
 }
